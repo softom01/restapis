0345d65825543cd48d4ebf824704d9fbff02c6a6
softom01 <lawalzainabad@gmail.com>
2023-06-14 14:13:07 +0100

    blocks

---
+++ blocks.py
@@ -0,0 +1,10 @@
+name = "Ade"
+password = "Atlass1an"
+
+if name == "Ade":
+    print("Hello Ade")
+    if password == "Atlass1an":
+        print("Welcome, Ade")
+    else:
+        print("Wrong password")
+
